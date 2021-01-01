63e72ff2430cce2c46f9b49eb93912e70e68ffc6
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-01-01 07:25:31 -0800

    I

---
+++ _/S30/testing-03-organizing-tests/test/auth-middleware.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+
+const authMiddleware = require('../middleware/is-auth');
+
+describe('Auth middleware', function() {
+  it('should throw an error if no authorization header is present', function() {
+    const req = {
+      get: function(headerName) {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', function() {
+    const req = {
+      get: function(headerName) {
+        return 'xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
